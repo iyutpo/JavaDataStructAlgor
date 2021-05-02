e7a29d4cf2d5393763c23851ceee14a874a5d745
iyutpo <y15653862659@outlook.com>
2021-05-01 21:28:33 -0400

    yinghai

--- yinghai/DataStruct/src/Tree/BinaryTreeArray.java
+++ yinghai/DataStruct/src/Tree/BinaryTreeArray.java
@@ -3,11 +3,12 @@ package Tree;
 /*
 * 一个二叉树和一个数组之间是可以相互转换的
 * 例如我们想将如下的二叉树 按照前序遍历 生成一个数组：
-*           1
-*       /       \
-*      2         3         ===>     [1, 2, 3, 4, 5, 6, 7]
-*   /    \    /     \
-*  4      5  6       7
+*           1             储存成                          前序遍历后
+*       /       \         ===>     [1, 2, 3, 4, 5, 6, 7]   ===>    [1, 2, 4, 5, 3, 6, 7]
+*      2         3                                       中序遍历后
+*   /    \    /     \                                      ===>    [4, 2, 5, 1, 6, 3, 7]
+*  4      5  6       7                                   后序遍历后
+*                                                          ===>    [4, 5, 2, 6, 7, 3, 1]
 * BST 中第 n 个元素的左子节点为 2 * n + 1
 *       第 n 个元素的右子节点为 2 * n + 2
 *       第 n 个元素的父节点为 (n - 1) / 2
@@ -18,7 +19,12 @@ public class BinaryTreeArray {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 5, 6, 7};
         BinaryTreeArrayDemo btArray = new BinaryTreeArrayDemo(arr);
-        btArray.preOrder(0 );
+        System.out.println("Pre order: ");
+        btArray.preOrder();
+        System.out.println("In order: ");
+        btArray.inOrder();
+        System.out.println("Post order: ");
+        btArray.postOrder();
     }
 }
 
@@ -28,6 +34,18 @@ class BinaryTreeArrayDemo {
 
     public BinaryTreeArrayDemo(int[] arr) {
         this.arr = arr;
+    }
+
+    public void preOrder() {
+        this.preOrder(0);
+    }
+
+    public void inOrder() {
+        this.inOrder(0);
+    }
+
+    public void postOrder() {
+        this.postOrder(0);
     }
 
     public void preOrder(int index) {
@@ -41,6 +59,32 @@ class BinaryTreeArrayDemo {
         if ((index * 2 + 2) < arr.length) {
             preOrder(2 * index + 2);
         }
+    }
+
+    public void inOrder(int index) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能进行前序遍历");
+        }
+        if ((index * 2 + 1) < arr.length) {
+            inOrder(2 * index + 1);
+        }
+        System.out.println(arr[index]);
+        if ((index * 2 + 2) < arr.length) {
+            inOrder(2 * index + 2);
+        }
+    }
+
+    public void postOrder(int index) {
+        if (arr == null || arr.length == 0) {
+            System.out.println("数组为空，不能进行前序遍历");
+        }
+        if ((index * 2 + 1) < arr.length) {
+            postOrder(2 * index + 1);
+        }
+        if ((index * 2 + 2) < arr.length) {
+            postOrder(2 * index + 2);
+        }
+        System.out.println(arr[index]);
     }
 }
 
---
+++ yinghai/DataStruct/src/Tree/ThreadBinaryTree.java
@@ -0,0 +1,9 @@
+package Tree;
+
+/*
+* threaded binary tree 在 该视频中提及： https://www.youtube.com/watch?v=DpzTv1IFR_o&list=PLmOn9nNkQxJFvyhDYx0ya4F75uTtUHA_f&index=103
+* 暂时先不具体实现
+* */
+
+public class ThreadBinaryTree {
+}
