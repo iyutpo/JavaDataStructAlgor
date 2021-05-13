fecc93d2d9c19c0b3a7b12d6645ac8935580b379
iyutpo <y15653862659@outlook.com>
2021-05-13 14:59:36 -0400

    yinghai

---
+++ yinghai/Algorithm/src/Floyd/Floyd.java
@@ -0,0 +1,11 @@
+package Floyd;
+
+/*
+* Floyd Algorithm： 与Dijkstra's Algorithm一样，也是用于计算给定加权图中顶点之间最短路径的算法。
+* 而与Dijkstra's Algorithm不同的是，Dijkstra's Algorithm计算的是某个特定顶点到其他各个顶点之间的最短距离（即单源最短路径）
+* Floyd Algorithm则是以图中所有顶点作为出发点，依次计算每个顶点到其余各个顶点之间的最短路径（即多源最短路径）
+*
+* 受限于文本格式，我们还是在 Floyd.md中详细讲解Floyd Algorithm
+* */
+public class Floyd {
+}
